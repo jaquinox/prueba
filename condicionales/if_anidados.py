8ed3d82c5ef5363a6e7d34dc5bf9534d4510bfdf
jaquinox <jaquinox123@gmail.com>
2024-05-18 15:48:58 -0400

    condicionales

---
+++ condicionales/if_anidados.py
@@ -0,0 +1,13 @@
+dia = "Lunes"
+
+estas_enfermo = False
+
+if dia == "Lunes":
+    print("Hoy voy al colegio")
+    
+    if estas_enfermo == False:
+        print("No estoy enfermo, voy a poder ir al colgio")
+    else:
+          print("pero no ire al colegio porque  estoy enfermo")  
+else:
+     print("hoy no es lunes, no hay clases")          
