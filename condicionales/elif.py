ff65ec640aae387c9b70b881adbf84b8b749db6d
jaquinox <jaquinox123@gmail.com>
2024-05-18 15:53:36 -0400

    modificaciones condicionales

---
+++ condicionales/elif.py
@@ -0,0 +1,13 @@
+dia = 2
+
+if dia == 1:
+    print("Hoy es Lunes")
+
+elif dia == 2:
+    print("Hoy es Martes")
+
+elif dia == 3:
+    print("Hoy es Miércoles")
+
+else:
+    print("No hay trabajo")
